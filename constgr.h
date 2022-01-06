15f654a8ad55c91c11a4c9b84f9af4f741de2089
CrabOfCrabs <95616091+CrabOfCrabs@users.noreply.github.com>
2022-01-06 23:43:37 +0100

    Add files via upload

---
+++ constgr.h
@@ -0,0 +1,257 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/ioctl.h>
+#include <stdbool.h>
+#include <math.h>
+#include <curses.h>
+
+
+// left handed system
+
+
+/*	
+	STRUCTURES FOR GRAPHICS
+*/	
+
+//new data type for a 2d point cords
+typedef struct points{double x;double y;double z;double w;}point;
+double dot(point p1,point p2);point subp(point p1,point p2);point divp(point p1,point p2);point mulpn(point p,double d);point normp(point p);double magp(point p);bool chckp(point p1,point p2);point crossp(point U, point V);//declarations of point functions
+//data type of a line
+typedef struct lines{point p1;point p2;}line;
+//data type of a triangle
+typedef struct triangle{point p1;point p2;point p3;}tri;
+tri amktri(double tarr[8]);point trinorm(tri t);
+//data for mesh contains all points and tris with pointers to poiters to tris and pointers to pointers to poitnts define with mesh.pa = calloc(size,sizeof(point*)) same for tris
+typedef struct meshes{tri** ta; point** pa;}mesh;
+
+typedef struct mat4{double m[4][4];}mat4;
+
+
+
+/*	
+	HARD CODED MATRICES
+*/	
+
+mat4 idm(){
+	mat4 pm;pm.m[0][1] = 0;pm.m[0][2] = 0;pm.m[0][3] = 0;pm.m[1][0] = 0;pm.m[1][2] = 0;pm.m[1][3] = 0;pm.m[2][0] = 0;pm.m[2][1] = 0;pm.m[2][3] = 0;
+	
+	pm.m[0][0] = 1;
+	pm.m[1][1] = 1;
+	pm.m[2][2] = 1;
+	pm.m[3][3] = 1;
+	pm.m[3][0] = 0;
+	pm.m[3][1] = 0;
+	pm.m[3][2] = 0;
+
+return pm;}
+mat4 spos(point pos){
+	mat4 pm;pm.m[0][1] = 0;pm.m[0][2] = 0;pm.m[0][3] = 0;pm.m[1][0] = 0;pm.m[1][2] = 0;pm.m[1][3] = 0;pm.m[2][0] = 0;pm.m[2][1] = 0;pm.m[2][3] = 0;
+	
+	pm.m[0][0] = 1;
+	pm.m[1][1] = 1;
+	pm.m[2][2] = 1;
+	pm.m[3][3] = 1;
+	pm.m[3][0] = pos.x;
+	pm.m[3][1] = pos.y;
+	pm.m[3][2] = pos.z;
+
+return pm;}mat4 inspos(point pos){
+	mat4 pm;pm.m[0][1] = 0;pm.m[0][2] = 0;pm.m[0][3] = 0;pm.m[1][0] = 0;pm.m[1][2] = 0;pm.m[1][3] = 0;pm.m[2][0] = 0;pm.m[2][1] = 0;pm.m[2][3] = 0;
+	
+	pm.m[0][0] = 1;
+	pm.m[1][1] = 1;
+	pm.m[2][2] = 1;
+	pm.m[3][3] = 1;
+	pm.m[3][0] = pos.x;
+	pm.m[3][1] = pos.y;
+	pm.m[3][2] = pos.z;
+
+return pm;}mat4 pscale(point scale){
+	mat4 pm;pm.m[0][1] = 0;pm.m[0][2] = 0;pm.m[0][3] = 0;pm.m[1][0] = 0;pm.m[1][2] = 0;pm.m[1][3] = 0;pm.m[2][0] = 0;pm.m[2][1] = 0;pm.m[2][3] = 0;pm.m[3][0] = 0;pm.m[3][1] = 0;pm.m[3][2] = 0;
+	
+	pm.m[0][0] = scale.x;
+	pm.m[1][1] = scale.y;
+	pm.m[2][2] = scale.z;
+	pm.m[3][3] = 1;
+
+return pm;}mat4 crmx(double theta){
+	mat4 pm;pm.m[0][1] = 0;pm.m[0][2] = 0;pm.m[0][3] = 0;pm.m[1][0] = 0;pm.m[1][3] = 0;pm.m[2][0] = 0;pm.m[2][3] = 0;pm.m[3][0] = 0;pm.m[3][1] = 0;pm.m[3][2] = 0;
+	
+	pm.m[0][0] = 1;
+	pm.m[1][1] = cos(theta);
+	pm.m[1][2] = sin(theta);
+	pm.m[2][1] = -sin(theta);
+	pm.m[2][2] = cos(theta);
+	pm.m[3][3] = 1;
+
+return pm;}mat4 crmy(double theta){
+	mat4 pm;pm.m[0][1] = 0;pm.m[0][3] = 0;pm.m[1][0] = 0;pm.m[1][2] = 0;pm.m[1][3] = 0;pm.m[2][1] = 0;pm.m[2][3] = 0;pm.m[3][0] = 0;pm.m[3][1] = 0;pm.m[3][2] = 0;
+	
+	pm.m[1][1] = 1;
+	pm.m[0][0] = cos(theta);
+	pm.m[2][0] = sin(theta);
+	pm.m[0][2] = -sin(theta);
+	pm.m[2][2] = cos(theta);
+	pm.m[3][3] = 1;
+
+return pm;}mat4 crmz(double theta){
+	mat4 pm;pm.m[0][2] = 0;pm.m[0][3] = 0;pm.m[1][2] = 0;pm.m[1][3] = 0;pm.m[2][0] = 0;pm.m[2][1] = 0;pm.m[2][3] = 0;pm.m[3][0] = 0;pm.m[3][1] = 0;pm.m[3][2] = 0;
+
+	pm.m[0][0] = cos(theta);
+	pm.m[0][1] = sin(theta);
+	pm.m[1][0] = -sin(theta);
+	pm.m[1][1] = cos(theta);
+	pm.m[2][2] = 1;
+	pm.m[3][3] = 1;
+
+return pm;}mat4 camtr(point eye,point cen,point up){//tri p1 = eye , p2 = center ,p3 = upvector
+	mat4 pm;pm.m[0][3] = 0;pm.m[1][3] = 0;pm.m[2][3] = 0;	
+
+	point zc = normp(subp(cen,eye));
+	point xc = normp(subp(up,mulpn(zc,dot(up,zc))));
+	point oxc = normp(crossp(up,zc));
+	point yc = crossp(xc,zc);
+
+	pm.m[0][0] = yc.x;
+	pm.m[1][0] = xc.x;
+	pm.m[2][0] = zc.x;
+	pm.m[0][1] = yc.y;
+	pm.m[1][1] = xc.y;
+	pm.m[2][1] = zc.y;
+	pm.m[0][2] = yc.z;
+	pm.m[1][2] = xc.z;
+	pm.m[2][2] = zc.z;
+	pm.m[3][0] = eye.x;
+	pm.m[3][1] = eye.y;
+	pm.m[3][2] = eye.z;
+	pm.m[3][3] = 1;/*
+	pm.m[0][0] = xc.x;
+	pm.m[0][1] = yc.x;
+	pm.m[0][2] = zc.x;
+	pm.m[1][0] = xc.y;
+	pm.m[1][1] = yc.y;
+	pm.m[1][2] = zc.y;
+	pm.m[2][0] = xc.z;
+	pm.m[2][1] = yc.z;
+	pm.m[2][2] = zc.z;
+	pm.m[3][0] = -xc.x*eye.x - xc.y*eye.y - xc.z*eye.z;
+	pm.m[3][1] = -yc.x*eye.x - yc.y*eye.y - xc.z*eye.z;
+	pm.m[3][2] = -zc.x*eye.x - zc.y*eye.y - zc.z*eye.z;
+	pm.m[3][3] = 1;*/
+return pm;}mat4 pmm(point screen){
+	mat4 pm;pm.m[0][1] = 0;pm.m[0][2] = 0;pm.m[0][3] = 0;pm.m[1][2] = 0;pm.m[1][3] = 0;pm.m[2][0] = 0;pm.m[2][1] = 0;pm.m[2][3] = 0;pm.m[3][0] = 0;pm.m[3][1] = 0;pm.m[3][2] = 0;
+	double f    = 0.1;
+	double px   = screen.x/10000;
+	double py   = screen.y/10000;
+	double offx = screen.x;
+	double offy = screen.y;
+	double skew = 0;
+
+	pm.m[0][0] = (f*screen.x)/(2*px);
+	pm.m[1][0] = skew;
+	pm.m[1][1] = (f*screen.y)/(2*py);
+	pm.m[2][2] = -1;
+	pm.m[3][3] = 1;
+
+return pm;}mat4 ppm(point screen){
+	mat4 pm;pm.m[0][1] = 0;pm.m[0][2] = 0;pm.m[0][3] = 0;pm.m[1][0] = 0;pm.m[1][2] = 0;pm.m[1][3] = 0;pm.m[2][0] = 0;pm.m[2][1] = 0;pm.m[3][0] = 0;pm.m[3][1] = 0;
+	double near = 0.1;
+	double far = 1000;
+	double fov = 30;
+	double asprt = screen.y/screen.x;
+	double fovr = 1 / tan(fov * 0.5 / 180 * 3.14159);
+
+	pm.m[0][0] = asprt * fovr;
+	pm.m[1][1] = fovr;
+	pm.m[2][2] = far / (far - near);
+	pm.m[2][3] = 1;
+	pm.m[3][2] = (-far * near) / (far - near);
+	pm.m[3][3] = 0;
+return pm;}mat4 symppm(point screen){
+	mat4 pm;pm.m[0][1] = 0;pm.m[0][2] = 0;pm.m[0][3] = 0;pm.m[1][0] = 0;pm.m[1][2] = 0;pm.m[1][3] = 0;pm.m[2][0] = 0;pm.m[2][1] = 0;pm.m[3][0] = 0;pm.m[3][1] = 0;
+	double near = 0.1;
+	double far = 100;
+	double fov = 45;
+	double asprt = screen.x/screen.y;
+	double fovr = 1/(tan(fov/2));
+
+	pm.m[0][0] = fovr/asprt;
+	pm.m[1][1] = fovr;
+	pm.m[2][2] = -(far+near) / (far - near);
+	pm.m[2][3] = -1;
+	pm.m[3][2] = -(2*far * near) / (far - near);
+	pm.m[3][3] = 0;
+return pm;}
+mat4 asymppm(point screen){
+	mat4 pm;pm.m[0][1] = 0;pm.m[0][2] = 0;pm.m[0][3] = 0;pm.m[1][0] = 0;pm.m[1][2] = 0;pm.m[1][3] = 0;pm.m[2][0] = 0;pm.m[2][1] = 0;pm.m[3][0] = 0;pm.m[3][1] = 0;
+	double near = 0.1;
+	double far = 1000;
+	double fov = 45;
+	double asprt = screen.x/screen.y;
+	double fovr = 1/(tan(fov/2));
+
+	pm.m[0][0] = fovr/asprt;
+	pm.m[1][1] = fovr;
+	pm.m[2][2] = -(far+near) / (far - near);
+	pm.m[2][3] = -1;
+	pm.m[3][2] = -(2*far * near) / (far - near);
+	pm.m[3][3] = 0;
+return pm;}
+mat4 pvm(double z){
+	mat4 pm;pm.m[0][1] = 0;pm.m[0][2] = 0;pm.m[0][3] = 0;pm.m[1][0] = 0;pm.m[1][2] = 0;pm.m[1][3] = 0;pm.m[2][0] = 0;pm.m[2][1] = 0;pm.m[2][3] = 0;pm.m[3][0] = 0;pm.m[3][1] = 0;pm.m[3][2] = 0;
+	pm.m[0][0]=1/z;
+	pm.m[1][1]=1/z;
+	pm.m[2][2]=1;
+	pm.m[3][3]=1;
+return pm;}
+mat4 voff(point off){
+	mat4 pm;pm.m[0][1] = 0;pm.m[0][2] = 0;pm.m[0][3] = 0;pm.m[1][0] = 0;pm.m[1][2] = 0;pm.m[1][3] = 0;pm.m[2][0] = 0;pm.m[2][1] = 0;pm.m[2][3] = 0;pm.m[3][2] = 0;
+	pm.m[0][0] = 1;
+	pm.m[1][1] = -1;
+	pm.m[2][2] = 1;
+	pm.m[3][3] = 1;
+	pm.m[3][0] = off.x;
+	pm.m[3][1] = off.y;
+return pm;}
+
+
+
+/*	
+	SHAPE TEMPLATES
+*/	
+
+//predefined square with size as edge lenght its center will be at 0,0,0 	
+void mkcube(tri *obj,double size){
+	
+	double smax = size/2;
+	double smin = -size/2;
+	double t1[9]={ smin,smin,smin,smin,smax,smin,smax,smax,smin };
+	double t2[9]={ smin,smin,smin,smax,smax,smin,smax,smin,smin };
+	double t3[9]={ smax,smin,smin,smax,smax,smin,smax,smax,smax };
+	double t4[9]={ smax,smin,smin,smax,smax,smax,smax,smin,smax};
+	double t5[9]={ smax,smin,smax,smax,smax,smax,smin,smax,smax };
+	double t6[9]={ smax,smin,smax,smin,smax,smax,smin,smin,smax };
+	double t7[9]={ smin,smin,smax,smin,smax,smax,smin,smax,smin };
+	double t8[9]={ smin,smin,smax,smin,smax,smin,smin,smin,smin };
+	double t9[9]={ smin,smax,smin,smin,smax,smax,smax,smax,smax };
+	double t10[9]={ smin,smax,smin,smax,smax,smax,smax,smax,smin };
+	double t11[9]={ smax,smin,smax,smin,smin,smax,smin,smin,smin };
+	double t12[9]={ smax,smin,smax,smin,smin,smin,smax,smin,smin };
+	                                                      	
+	obj[0] = amktri(t1);
+	obj[1] = amktri(t2);
+	obj[2] = amktri(t3);
+	obj[3] = amktri(t4);
+	obj[4] = amktri(t5);
+	obj[5] = amktri(t6);
+	obj[6] = amktri(t7);
+	obj[7] = amktri(t8);
+	obj[8] = amktri(t9);
+	obj[9] = amktri(t10);
+	obj[10] = amktri(t11);
+	obj[11] = amktri(t12);}
+
+
+
--- crlib.h
+++ crlib.h
@@ -6,140 +6,58 @@
 #include <stdbool.h>
 #include <math.h>
 #include <curses.h>
-
-// left handed system
-
+#include "constgr.h"
 
 
+/*	
+	HEADER CONSTANT //will be move to main toggles file in future
+*/
 
-//new data type for a 2d point cords
-typedef struct points{float x;float y;float z;}point;
-point subp(point p1,point p2);point normp(point p);float magp(point p);bool chckp(point p1,point p2);//declarations of point functions
-//data type of a line
-typedef struct lines{point p1;point p2;}line;
-//data type of a triangle
-typedef struct triangle{point p1;point p2;point p3;}tri;
-point trinorm(tri t);
-//data for mesh contains all points and tris with pointers to poiters to tris and pointers to pointers to poitnts define with mesh.pa = calloc(size,sizeof(point*)) same for tris
-typedef struct meshes{tri** ta; point** pa;}mesh;
+bool inToggle = false;
 
-typedef struct mat4{float m[4][4];}mat4;
+
+/*	
+	GENERAL PURPOSE FUNCTIONS //will be move to general use header
+*/	
+
+double clamp(double n,double max,double min){if(n<min){n=min;}else if(n>max){n=max;}return n;}
 
 
 
-mat4 campos(point campos){
-	mat4 pm;pm.m[0][1] = 0;pm.m[0][2] = 0;pm.m[0][3] = 0;pm.m[1][0] = 0;pm.m[1][2] = 0;pm.m[1][3] = 0;pm.m[2][0] = 0;pm.m[2][1] = 0;pm.m[2][3] = 0;
-	pm.m[0][0] = 1;
-	pm.m[1][1] = 1;
-	pm.m[2][2] = 1;
-	pm.m[3][3] = 1;
-	pm.m[3][0] = -campos.x;
-	pm.m[3][1] = -campos.y;
-	pm.m[3][2] = -campos.z;
-return pm;}mat4 crmx(float theta){
-	mat4 pm;pm.m[0][1] = 0;pm.m[0][2] = 0;pm.m[0][3] = 0;pm.m[1][0] = 0;pm.m[1][3] = 0;pm.m[2][0] = 0;pm.m[2][3] = 0;pm.m[3][0] = 0;pm.m[3][1] = 0;pm.m[3][2] = 0;
-	pm.m[0][0] = 1;
-	pm.m[1][1] = cos(theta);
-	pm.m[1][2] = sin(theta);
-	pm.m[2][1] = -sin(theta);
-	pm.m[2][2] = cos(theta);
-	pm.m[3][3] = 1;
-return pm;}mat4 crmy(float theta){
-	mat4 pm;pm.m[0][1] = 0;pm.m[0][3] = 0;pm.m[1][0] = 0;pm.m[1][2] = 0;pm.m[1][3] = 0;pm.m[2][1] = 0;pm.m[2][3] = 0;pm.m[3][0] = 0;pm.m[3][1] = 0;pm.m[3][2] = 0;
-	pm.m[1][1] = 1;
-	pm.m[0][0] = cos(theta);
-	pm.m[2][0] = sin(theta);
-	pm.m[0][2] = -sin(theta);
-	pm.m[2][2] = cos(theta);
-	pm.m[3][3] = 1;
-
-return pm;}mat4 crmz(float theta){
-	mat4 pm;pm.m[0][2] = 0;pm.m[0][3] = 0;pm.m[1][2] = 0;pm.m[1][3] = 0;pm.m[2][0] = 0;pm.m[2][1] = 0;pm.m[2][3] = 0;pm.m[3][0] = 0;pm.m[3][1] = 0;pm.m[3][2] = 0;
-	pm.m[0][0] = cos(theta);
-	pm.m[0][1] = sin(theta);
-	pm.m[1][0] = -sin(theta);
-	pm.m[1][1] = cos(theta);
-	pm.m[2][2] = 1;
-	pm.m[3][3] = 1;
-return pm;}
-mat4 ppm(point screen){
-	mat4 pm;pm.m[0][1] = 0;pm.m[0][2] = 0;pm.m[0][3] = 0;pm.m[1][2] = 0;pm.m[1][3] = 0;pm.m[2][0] = 0;pm.m[2][1] = 0;pm.m[2][3] = 0;pm.m[3][0] = 0;pm.m[3][1] = 0;pm.m[3][2] = 0;
-	float f    = 0.1;
-	float px   = screen.x/10000;
-	float py   = screen.y/10000;
-	float offx = screen.x;
-	float offy = screen.y;
-	float skew = 0;
-
-	pm.m[0][0] = (f*screen.x)/(2*px);
-	pm.m[1][0] = skew;
-	pm.m[1][1] = (f*screen.y)/(2*py);
-	pm.m[2][2] = -1;
-	pm.m[3][3] = 1;
-return pm;}
-mat4 pvm(float z){
-	mat4 pm;pm.m[0][1] = 0;pm.m[0][2] = 0;pm.m[0][3] = 0;pm.m[1][0] = 0;pm.m[1][2] = 0;pm.m[1][3] = 0;pm.m[2][0] = 0;pm.m[2][1] = 0;pm.m[2][3] = 0;pm.m[3][0] = 0;pm.m[3][1] = 0;pm.m[3][2] = 0;
-	pm.m[0][0]=1/z;
-	pm.m[1][1]=1/z;
-	pm.m[2][2]=1;
-	pm.m[3][3]=1;
-return pm;}
-mat4 voff(point off){
-	mat4 pm;pm.m[0][1] = 0;pm.m[0][2] = 0;pm.m[0][3] = 0;pm.m[1][0] = 0;pm.m[1][2] = 0;pm.m[1][3] = 0;pm.m[2][0] = 0;pm.m[2][1] = 0;pm.m[2][3] = 0;pm.m[3][2] = 0;
-	pm.m[0][0] = 1;
-	pm.m[1][1] = -1;
-	pm.m[2][2] = 1;
-	pm.m[3][3] = 1;
-	pm.m[3][0] = off.x;
-	pm.m[3][1] = off.y;
-return pm;}
-
-
-point multm(point p,mat4 m){
-	point o;
-	o.x = p.x * m.m[0][0] + p.y * m.m[1][0] + p.z * m.m[2][0] + m.m[3][0];
-	o.y = p.x * m.m[0][1] + p.y * m.m[1][1] + p.z * m.m[2][1] + m.m[3][1];
-	o.z = p.x * m.m[0][2] + p.y * m.m[1][2] + p.z * m.m[2][2] + m.m[3][2];
-	float w = p.x * m.m[0][3] + p.y * m.m[1][3] + p.z * m.m[2][3] + m.m[3][3];
-	if (w != 0){o.x /= w; o.y /= w; o.z /= w;}
-	return o;}
-
-
-
-
-float clamp(float n,float max,float min){if(n<min){n=min;}else if(n>max){n=max;}return n;}
-
-
-
-
+/*	
+	POINT OPERATION FUNCTIONS
+*/	
 
 //functions for point operations
-point mkp(float x,float y,float z){point p = {x,y,z};return p;} //makes a point
+point mkp(double x,double y,double z){point p = {x,y,z,1};return p;} //makes a point
 
 void swapp(point *p1,point *p2){point tmp = *p1;*p1 = *p2;*p2 = tmp;}
 
 bool chckp(point p1,point p2){if(p1.x==p2.x || p1.y==p2.y || (p1.z==p2.z && p1.z!=0 && p2.z!=0)){return true;}return false;} //checks if points are the same or not
 
-float magp(point p){float mg = sqrt((p.x*p.x) + (p.y*p.y) + (p.z*p.z));return mg;}
-float sign (point p1, point p2, point p3){return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y);}
-float dot(point p1,point p2){float dot = (p1.x*p2.x) + (p1.y*p2.y) + (p1.z*p2.z);return dot;}
+double magp(point p){double mg = sqrt((p.x*p.x) + (p.y*p.y) + (p.z*p.z));return mg;}
+double sign (point p1, point p2, point p3){return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y);}
+double dot(point p1,point p2){double dot = (p1.x*p2.x) + (p1.y*p2.y) + (p1.z*p2.z);return dot;}
 
 point subp(point p1,point p2){point p3=mkp(p1.x - p2.x, p1.y - p2.y, p1.z - p2.z);return p3;} //subctracts second point from the first
 point addp(point p1,point p2){point p3=mkp(p1.x + p2.x, p1.y + p2.y, p1.z + p2.z);return p3;}
+point divp(point p1,point p2){point p3=mkp(p1.x / p2.x, p1.y / p2.y, p1.z / p2.z);return p3;}
+point mulp(point p1,point p2){point p3=mkp(p1.x * p2.x, p1.y * p2.y, p1.z * p2.z);return p3;}
 
-point subpn(point p,float d){point p1=mkp(p.x-d,p.y-d,p.z-d); return p1;}
-point addpn(point p,float d){point p1=mkp(p.x+d,p.y+d,p.z+d); return p1;}
-point divpn(point p,float d){point p1=mkp(p.x/d,p.y/d,p.z/d); return p1;}
-point mulpn(point p,float d){point p1=mkp(p.x*d,p.y*d,p.z*d); return p1;}
+point subpn(point p,double d){point p1=mkp(p.x-d,p.y-d,p.z-d); return p1;}
+point addpn(point p,double d){point p1=mkp(p.x+d,p.y+d,p.z+d); return p1;}
+point divpn(point p,double d){point p1=mkp(p.x/d,p.y/d,p.z/d); return p1;}
+point mulpn(point p,double d){point p1=mkp(p.x*d,p.y*d,p.z*d); return p1;}
 
-point chngx(point p,float d){point p1=mkp(d,p.y,p.z); return p1;}
-point chngy(point p,float d){point p1=mkp(p.x,d,p.z); return p1;}
-point chngz(point p,float d){point p1=mkp(p.x,p.y,d); return p1;}
+void chngx(point *p,double d){ p->x = d;}
+void chngy(point *p,double d){ p->y = d;}
+void chngz(point *p,double d){ p->z = d;}
+void chngw(point *p,double d){ p->w = d;}
 
 point negp(point p){point np = mkp(-p.x,-p.y,-p.z);return np;}
 
 point normp(point p){
-	float mg = magp(p);
+	double mg = magp(p);
 	point pn = mkp(p.z/mg,p.y/mg,p.z/mg);
 	return pn;}//normailize a vector
 point tricenp(tri t){
@@ -148,25 +66,56 @@ point tricenp(tri t){
 	return p;}
 point crossp(point U ,point V){point c = mkp((U.y*V.z)-(U.z*V.y), (U.z*V.x)-(U.x*V.z), (U.x*V.y)-(U.y*V.x));return c;}
 
+point normdcp(point p,point screen){point pw = {p.x/p.w,p.y/p.w,p.z/p.w,0};point op = mkp(( screen.x / 2 )*((-1*pw.x)+1),(screen.y / 2 )*((-1*pw.y)+1),0.5*pw.z + 0.5);return op;}
+
+point multm(point p,mat4 m){
+	point o;
+	o.x = p.x * m.m[0][0] + p.y * m.m[1][0] + p.z * m.m[2][0] + p.w * m.m[3][0];
+	o.y = p.x * m.m[0][1] + p.y * m.m[1][1] + p.z * m.m[2][1] + p.w * m.m[3][1];
+	o.z = p.x * m.m[0][2] + p.y * m.m[1][2] + p.z * m.m[2][2] + p.w * m.m[3][2];
+	o.w = p.x * m.m[0][3] + p.y * m.m[1][3] + p.z * m.m[2][3] + p.w * m.m[3][3];
+	return o;}
+
+mat4 qinvm(mat4 m){
+		mat4 matrix;
+		matrix.m[0][0] = m.m[0][0]; matrix.m[0][1] = m.m[1][0]; matrix.m[0][2] = m.m[2][0]; matrix.m[0][3] = 0;
+		matrix.m[1][0] = m.m[0][1]; matrix.m[1][1] = m.m[1][1]; matrix.m[1][2] = m.m[2][1]; matrix.m[1][3] = 0;
+		matrix.m[2][0] = m.m[0][2]; matrix.m[2][1] = m.m[1][2]; matrix.m[2][2] = m.m[2][2]; matrix.m[2][3] = 0;
+		matrix.m[3][0] = -(m.m[3][0] * matrix.m[0][0] + m.m[3][1] * matrix.m[1][0] + m.m[3][2] * matrix.m[2][0]);
+		matrix.m[3][1] = -(m.m[3][0] * matrix.m[0][1] + m.m[3][1] * matrix.m[1][1] + m.m[3][2] * matrix.m[2][1]);
+		matrix.m[3][2] = -(m.m[3][0] * matrix.m[0][2] + m.m[3][1] * matrix.m[1][2] + m.m[3][2] * matrix.m[2][2]);
+		matrix.m[3][3] = 1;
+		return matrix;}
+mat4 multms(mat4 m1,mat4 m2){
+	mat4 matrix;
+		for (int c = 0; c < 4; c++)
+			for (int r = 0; r < 4; r++)
+				matrix.m[r][c] = m1.m[r][0] * m2.m[0][c] + m1.m[r][1] * m2.m[1][c] + m1.m[r][2] * m2.m[2][c] + m1.m[r][3] * m2.m[3][c];
+		return matrix;}
 
 
 
+
+/*	
+	LINE OPERATION FUNCTIONS
+*/	
 
 //functions for line opertaions p1 decided on y cordintate
 line mkline(point p1,point p2){if(p1.y>p2.y){line l =  {p1,p2};return l;}else{line l = {p2,p1};return l;}} // makes a point
 
-float inpolx(line l,float y){float x = l.p1.x + (l.p2.x - l.p1.x)*(y - l.p1.y)/(l.p2.y - l.p1.y);return x;}
+double inpolx(line l,double y){double x = l.p1.x + (l.p2.x - l.p1.x)*(y - l.p1.y)/(l.p2.y - l.p1.y);return x;}
 
 
 
 
-
-
+/*	
+	TRI OPERATION FUNCTIONS
+*/	
 
 //functions for triangle operations
 tri mktri(point p1,point p2,point p3){tri t = {p1,p2,p3};return t;}
 //makes tri with 9 numbers
-tri amktri(float tarr[9]){tri t = mktri(mkp(tarr[0],tarr[1],tarr[2]),mkp(tarr[3],tarr[4],tarr[5]),mkp(tarr[6],tarr[7],tarr[8]));return t;}
+tri amktri(double tarr[9]){tri t = mktri(mkp(tarr[0],tarr[1],tarr[2]),mkp(tarr[3],tarr[4],tarr[5]),mkp(tarr[6],tarr[7],tarr[8]));return t;}
 
 void lnfy(tri t,line *l){
 	line ll1 = mkline(t.p1,t.p2);
@@ -182,7 +131,7 @@ void offsett(tri *t,point off){
 
 bool PointInTriangle (tri t, point p){
 	point p1 = t.p1, p2 = t.p2, p3 = t.p3;
-	float d1, d2, d3;
+	double d1, d2, d3;
 	bool has_neg, has_pos;
 	d1 = sign(p, p1, p2);
 	d2 = sign(p, p2, p3);
@@ -191,10 +140,10 @@ bool PointInTriangle (tri t, point p){
 	has_pos = (d1 > 0) || (d2 > 0) || (d3 > 0);
 	return !(has_neg && has_pos);}
 
-float calcshade(tri t,point s){
+double calcshade(tri t,point s){
 	point v = normp(s);
 	point tn = trinorm(t);
-	float out = dot(tn,v);
+	double out = dot(tn,v);
 	return out;}
 
 point trinorm(tri t){
@@ -211,60 +160,36 @@ tri psort(tri t){
 	if(tr.p1.y == tr.p2.y && tr.p1.x<tr.p2.x){ swapp(&tr.p1, &tr.p2); }
 	if(tr.p3.y == tr.p2.y && tr.p3.x<tr.p2.x){ swapp(&tr.p3, &tr.p2); }
 	return tr;}
-
-//mesh mkmesh(point &pa,tri &ta){mesh n;n.ta = calloc(sizeof ta / sizeof ta[0] ,sizeof(tri*));n.pa = calloc(sizeof pa /sizeof pa[0],sizeof(point*));return n;}
-
+//mesh mkmesh(point &pa,tri &ta){mesh n;n.ta = calloc(sizeof ta / sizeof ta[0] ,sizeof(tri*));n.pa = calloc(sizeof pa /sizeof pa[0],sizeof(point*));return n;} //i really need to implement meshes
 	
 
 
-
-//predefined object 	
-void mkcube(tri *obj,float size){
-	
-	float smax = size/2;
-	float smin = -size/2;
-	float t1[9]={ smin,smin,smin,smin,smax,smin,smax,smax,smin };
-	float t2[9]={ smin,smin,smin,smax,smax,smin,smax,smin,smin };
-	float t3[9]={ smax,smin,smin,smax,smax,smin,smax,smax,smax };
-	float t4[9]={ smax,smin,smin,smax,smax,smax,smax,smin,smax};
-	float t5[9]={ smax,smin,smax,smax,smax,smax,smin,smax,smax };
-	float t6[9]={ smax,smin,smax,smin,smax,smax,smin,smin,smax };
-	float t7[9]={ smin,smin,smax,smin,smax,smax,smin,smax,smin };
-	float t8[9]={ smin,smin,smax,smin,smax,smin,smin,smin,smin };
-	float t9[9]={ smin,smax,smin,smin,smax,smax,smax,smax,smax };
-	float t10[9]={ smin,smax,smin,smax,smax,smax,smax,smax,smin };
-	float t11[9]={ smax,smin,smax,smin,smin,smax,smin,smin,smin };
-	float t12[9]={ smax,smin,smax,smin,smin,smin,smax,smin,smin };
-	                                                      	
-	obj[0] = amktri(t1);
-	obj[1] = amktri(t2);
-	obj[2] = amktri(t3);
-	obj[3] = amktri(t4);
-	obj[4] = amktri(t5);
-	obj[5] = amktri(t6);
-	obj[6] = amktri(t7);
-	obj[7] = amktri(t8);
-	obj[8] = amktri(t9);
-	obj[9] = amktri(t10);
-	obj[10] = amktri(t11);
-	obj[11] = amktri(t12);}
-
-
+/*	
+	RENDER FUNCTIONS OPERATION FUNCTIONS
+*/	
 
 //rendering stuff or something
 point projp(point p,point xyzr,point camor,point camoff){
-	point cor = multm(p,campos(camor)); //projection with camera offset
+	point cor = multm(p,spos(camor)); //projection with camera offset
 	point camrot = multm(multm(multm(cor,crmz(xyzr.z)),crmy(xyzr.y)),crmx(xyzr.x));//projection with camera rotation
-	point coff = multm(camrot,campos(camoff));
+	point coff = multm(camrot,spos(camoff));
 	return coff;}
+	
+point projpw(point p,point eye,point cen ,point up,point screen){
+	point camrot = multm(p,multms(multms(multms(multms(multms(spos(mkp(0,0,0)),pscale(mkp(1,1,1))),crmz(0)),crmx(0)),spos(mkp(0,0,0))),idm()));
+	point cor = multm(camrot,qinvm(camtr(eye,cen,up))); //projection with camera offset
+	point coff = multm(cor,ppm(screen));	
+	point enp = normdcp(coff,screen);
+	return enp;}
+
 point rend(point p,point screen){
 
-	point prp = multm(p,ppm(screen)); //final point projection
+	point prp = multm(p,pmm(screen)); //final point projection
 	point pvp = multm(prp,pvm(prp.z));
 	point fop = multm(pvp,voff(mkp(screen.x/2,screen.y/2,0)));
 	return fop;}
 
-tri rent(tri t,float r){
+tri rent(tri t,double r){
 	point rc = mkp(r,r/2,r/3);
 	point pc = mkp(0,0,0);
 	point co = mkp(0,0,5);
@@ -274,21 +199,28 @@ tri projt(tri t,point screen){
 	tri t1 = mktri(rend(t.p1,screen),rend(t.p2,screen),rend(t.p3,screen));
 	return t1;}
 
+tri rentsym(tri t,point screen,double r){
+	point rc = mkp(1.5,r/10,1.5);
+	point pc = mkp(0,0,0);
+	point co = mkp(0,-1,0);
+
+	tri t1 = mktri(projpw(t.p1,rc,pc,co,screen),projpw(t.p2,rc,pc,co,screen),projpw(t.p3,rc,pc,co,screen));
+	return t1;}
 
 
-
-
+/*	
+	DRAWING FUNCTIONS //will be moved to a seperate graphis handling library or 2d objects
+*/
 
 //scanline top and down triangle projection
-
 void chshaded(point p){
 	int offs = 0;
-	float y=p.y, x=p.x, lg=p.z; //z is shade val
+	double y=p.y, x=p.x, lg=p.z; //z is shade val
 	
 	if(lg>1){
 	mvprintw(y/2+offs,x+offs,"@");}
 	else if(lg<=1 && lg>0.9){
-	mvprintw(y/2+offs,x+offs,"b");}
+	mvprintw(y/2+offs,x+offs,"$");}
 	else if(lg<=0.9 && lg>0.8){
 	mvprintw(y/2+offs,x+offs,"#");}
 	else if(lg<=0.8 && lg>0.7){
@@ -302,41 +234,40 @@ void chshaded(point p){
 	else if(lg<=0.4 && lg>0.3){
 	mvprintw(y/2+offs,x+offs,":");}
 	else if(lg<=0.3 && lg>0){
-	mvprintw(y/2+offs,x+offs,".");}
-}
-
+	mvprintw(y/2+offs,x+offs,".");}}
 void drawline2d(point p1,point p2){
-	float dx, dy, p, x, y;int offs = 20;float x1 = p1.x;float x2 = p2.x;dy=0;float x2l;
+	double dx, dy, p, x, y;int offs = 20;double x1 = p1.x;double x2 = p2.x;dy=0;double x2l;
 	if(x1<=x2){x=x1;dx=x2-x1;x2l = x2;}else{x=x2;dx=x1-x2;x2l = x1;}y=p1.y;p=2*dy-dx;
-	float lg = p1.z;
+	double lg = p1.z;
 	while(x<x2l){
 		if(p>=0){chshaded(mkp(x,y,lg));y=y+1;p=p+2*dy-2*dx;}else{chshaded(mkp(x,y,lg));p=p+2*dy;}x=x+1;}}
-void drawtup(tri t,float lg){
+void drawtup(tri t,double lg){
 	point p1 = t.p1,p2 = t.p2,p3 = t.p3;
-	float invslope1 = (p2.x - p1.x) / (p2.y - p1.y);
-	float invslope2 = (p3.x - p1.x) / (p3.y - p1.y);
+	double invslope1 = (p2.x - p1.x) / (p2.y - p1.y);
+	double invslope2 = (p3.x - p1.x) / (p3.y - p1.y);
 	
-	float curx1 = p1.x;
-	float curx2 = p1.x;
-float offs = 20;
-	for(float Y = p1.y; Y <= p2.y; Y++){
+	double curx1 = p1.x;
+	double curx2 = p1.x;
+double offs = 20;
+	for(double Y = p1.y; Y <= p2.y; Y++){
 		drawline2d(mkp(curx1,Y,lg),mkp(curx2,Y,0));
 		curx1 += invslope1;
 		curx2 += invslope2;}}
-void drawtdown(tri t,float lg){
+void drawtdown(tri t,double lg){
 	point p1 = t.p1,p2 = t.p2,p3 = t.p3;
-	float invslope1 = (p3.x - p1.x) / (p3.y - p1.y);
-	float invslope2 = (p3.x - p2.x) / (p3.y - p2.y);
+	double invslope1 = (p3.x - p1.x) / (p3.y - p1.y);
+	double invslope2 = (p3.x - p2.x) / (p3.y - p2.y);
 	
-	float curx1 = p3.x;
-	float curx2 = p3.x;
-	float offs = 20;
-	for(float Y = p3.y; Y >p1.y; Y--){
+	double curx1 = p3.x;
+	double curx2 = p3.x;
+	double offs = 20;
+	for(double Y = p3.y; Y >p1.y; Y--){
 		drawline2d(mkp(curx1,Y,lg),mkp(curx2,Y,0));
 		curx1 -= invslope1;
 		curx2 -= invslope2;}}
-void scanln(tri t,float xr,float lg,point screen){
-	tri tr = projt(rent(t,xr),screen);
+void scanln(tri t,double xr,double lg,point screen){
+	//tri tr = projt(rent(t,xr),screen);
+	tri tr = rentsym(t,screen,xr);
 	if(tr.p2.y < tr.p1.y){ swapp(&tr.p2, &tr.p1); }if(tr.p3.y <tr.p1.y){ swapp(&tr.p3, &tr.p1); }if(tr.p3.y < tr.p2.y){ swapp(&tr.p3, &tr.p2); }
 	
 	if(tr.p3.y == tr.p2.y){drawtup(tr,lg);}

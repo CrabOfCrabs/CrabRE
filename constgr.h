c2f165d2fe31b2c3e4251821408cc09b1e02b556
CrabOfCrabs <95616091+CrabOfCrabs@users.noreply.github.com>
2022-02-12 15:16:03 +0100

    String

--- constgr.h
+++ constgr.h
@@ -193,7 +193,7 @@ void mkfile2(tri *obj){
 }
 
 void mkfile(tri *obj){
-	FILE *fp = fopen("testSuperPOLY (1).obj","r");
+	FILE *fp = fopen("testSuperPOLY-1.obj","r");
 
 	point pbuff[2028];
 	char lbuff[64];
